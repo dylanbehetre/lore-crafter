fc127e672fc9898cae241df10d76946150a94baf
Dylan BEHETRE <dylan.behetre@outlook.fr>
2025-06-24 15:17:49 +0200

    feat: add a tab to switch between reactive and template-driven form

--- lore-crafter-portal/src/app/universe/creation/universe-creation-form/universe-creation-form.component.ts
+++ lore-crafter-portal/src/app/universe/creation/universe-creation-form/universe-creation-form.component.ts
@@ -1,22 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {
   UniverseCreationTemplateDrivenFormComponent
 } from './universe-creation-template-driven-form/universe-creation-template-driven-form.component';
 import {CreatingUniverse} from './creating-universe.model';
+import {FormsModule} from '@angular/forms';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'lcp-universe-creation-form',
   imports: [
-    UniverseCreationTemplateDrivenFormComponent
+    UniverseCreationTemplateDrivenFormComponent,
+    MatTabsModule,
+    FormsModule
   ],
   templateUrl: './universe-creation-form.component.html',
   styleUrl: './universe-creation-form.component.css'
 })
-export class UniverseCreationFormComponent implements OnInit {
+export class UniverseCreationFormComponent {
 
-  protected creatingUniverse!: CreatingUniverse;
-
-  ngOnInit(): void {
-    this.creatingUniverse = new CreatingUniverse()
-  }
+  protected creatingUniverse: CreatingUniverse = new CreatingUniverse();
 }
